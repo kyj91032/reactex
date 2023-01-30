640d054109757a47532acccf4b80c53a482809df
sees77770 <sees77770@gmail.com>
2023-01-30 20:32:25 +0900

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom'; // 라우터
+
+import First from './album/First.js';
+import { useState } from 'react';
 
 function App() {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  let a = menuOpen ? 'active2' : ''
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul className={menuOpen ? 'active' : ''}>
+        <li><a class="title" href="/">Black Skirt</a></li>
+        <li><a href="1">정규 1집 [201]</a></li>
+        <li><a href="2">정규 2집 [Don't You Worry Baby]</a></li>
+        <li><a href="31">3집 PART 1 [TEAM BABY]</a></li>
+        <li><a href="32">3집 PART 2 [THIRSTY]</a></li>
+        <li><a href="33">3집 PART 3 [TEEN TROUBLES]</a></li>
+        <li></li>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className={"main "+ a}>
+              <button onClick={()=>{
+                setMenuOpen(menuOpen=>!menuOpen)
+              }}>{menuOpen ? 'open' : 'close'}</button>
+              안녕하세요
+            </div>
+          </>
+        }/>
+
+        <Route path="/1" element={
+          <First/>
+        }/>
+      </Routes>
+
+
     </div>
   );
 }
+
+
+
+
 
 export default App;
---
+++ src/album/First.js
@@ -0,0 +1,12 @@
+function First(){
+  return (
+    <>
+    <div class="main">
+      <div class="img"></div>
+      정규 1집 [201]
+    </div>
+    </>
+  )
+}
+
+export default First;
